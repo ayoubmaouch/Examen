b0f93715ab6ef259fe229d899e1aeab5f34e2b04
ayoubmaouch <ayoubmaouch@gmail.com>
2022-02-03 18:00:27 +0100

    Troisiéme modification

--- src/ClassExm.java
+++ src/ClassExm.java
@@ -3,6 +3,7 @@ public class ClassExm {
 
 	public static void main(String[] args) {
 		System.out.println("Bonjour monsieur Oubihi:\nc'est AYOUB MAOUCH de la 4 éme année ISI");
+		// ajout d'une modification test
 		//ce texet est un test seulement
 		
 		
