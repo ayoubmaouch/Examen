148e7bc7ad3e8f2b7021991b21293bca2ab49c20
ayoubmaouch <ayoubmaouch@gmail.com>
2022-02-03 17:50:25 +0100

    Deuxiéme commit du même projet "Projet-Exm"

--- src/ClassExm.java
+++ src/ClassExm.java
@@ -2,7 +2,10 @@
 public class ClassExm {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Teacher :");
+		System.out.println("Bonjour monsieur Oubihi:\nc'est AYOUB MAOUCH de la 4 éme année ISI");
+		//ce texet est un test seulement
+		
+		
 
 	}
 
