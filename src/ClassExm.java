736f265b68121e74a8742554a4355d7063d25f45
ayoubmaouch <ayoubmaouch@gmail.com>
2022-02-03 18:12:15 +0100

    Quatrième modéfication

--- src/ClassExm.java
+++ src/ClassExm.java
@@ -2,8 +2,7 @@
 public class ClassExm {
 
 	public static void main(String[] args) {
-		System.out.println("Bonjour monsieur Oubihi:\nc'est AYOUB MAOUCH de la 4 éme année ISI");
-		// ajout d'une modification test
+		System.out.println("Bonjour monsieur Oubihi:\nc'est AYOUB MAOUCH de la 4 éme année ISI modefication modefictaions");
 		//ce texet est un test seulement
 		
 		
